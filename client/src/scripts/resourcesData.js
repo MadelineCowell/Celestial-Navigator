3313b75d400f4fc19259c9d24ac0883a89856ae8
MadelineC <madeline.cowell@colorado.edu>
2020-12-15 14:36:17 -0700

    Pushing project to repo

---
+++ client/src/scripts/resourcesData.js
@@ -0,0 +1,8 @@
+export const resourceSeed = [
+   {        
+        image: "https://www.alohaastro.com/wp-content/uploads/2016/08/old-library.jpg",
+        title: "Astro Library",
+        text: "Learn to read your own stars with this online beginners course.",
+        link: "https://astrolibrary.org/lessons/"
+    }
+ ]
